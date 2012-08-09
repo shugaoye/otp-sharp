621e0fafc2bc1cd4065411eedf23cdf9103517a0
devin <devin@localhost>
2012-08-09 11:27:41 -0600

    Add a windows form app to use with the google authenticator mobile app to test TOTP implementation

---
+++ GoogleAuthenticatorTotpTest/GoogleAuthenticatorTotpTest.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace GoogleAuthenticatorTotpTest
+{
+    partial class GoogleAuthenticatorTotpTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelTotp = new System.Windows.Forms.Label();
+            this.timerUpdate = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::GoogleAuthenticatorTotpTest.Properties.Resources.QR;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(204, 207);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelTotp
+            // 
+            this.labelTotp.AutoSize = true;
+            this.labelTotp.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTotp.Location = new System.Drawing.Point(64, 257);
+            this.labelTotp.Name = "labelTotp";
+            this.labelTotp.Size = new System.Drawing.Size(70, 26);
+            this.labelTotp.TabIndex = 1;
+            this.labelTotp.Text = "label1";
+            // 
+            // timerUpdate
+            // 
+            this.timerUpdate.Enabled = true;
+            this.timerUpdate.Tick += new System.EventHandler(this.timerUpdate_Tick);
+            // 
+            // GoogleAuthenticatorTotpTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(240, 364);
+            this.Controls.Add(this.labelTotp);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "GoogleAuthenticatorTotpTest";
+            this.Text = "Google Authenticator Totp Test";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelTotp;
+        private System.Windows.Forms.Timer timerUpdate;
+    }
+}
+
---
+++ GoogleAuthenticatorTotpTest/GoogleAuthenticatorTotpTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OtpSharp;
+
+namespace GoogleAuthenticatorTotpTest
+{
+    public partial class GoogleAuthenticatorTotpTest : Form
+    {
+        /// <summary>
+        /// The test key provided in the RFC
+        /// </summary>
+        static readonly Totp totp = new Totp(new byte[] { 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x30 });
+        public GoogleAuthenticatorTotpTest()
+        {
+            InitializeComponent();
+            this.UpdateCode();
+        }
+
+        private void UpdateCode()
+        {
+            this.labelTotp.Text = totp.ComputeTotp(DateTime.UtcNow).ToString().PadLeft(6, '0');
+        }
+
+        private void timerUpdate_Tick(object sender, EventArgs e)
+        {
+            this.UpdateCode();
+        }
+    }
+}
