c5c25c7160b7566b5e35dce28696b3fd2b521558
devin <devin@localhost>
2012-08-10 20:35:27 -0600

    Stub in a key generation utility

---
+++ OtpSharp/KeyGeneration.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OtpSharp
+{
+    /// <summary>
+    /// Helpers to work with keys
+    /// </summary>
+    public class KeyGeneration
+    {
+        /// <summary>
+        /// Generate key of the specified length
+        /// </summary>
+        /// <param name="length">Key lenght</param>
+        /// <returns>The generated key</returns>
+        public static byte[] GenerateKey(int length)
+        {
+            byte[] key = new byte[length];
+            var rnd = System.Security.Cryptography.RandomNumberGenerator.Create();
+            rnd.GetBytes(key);
+            return key;
+        }
+
+        /// <summary>
+        /// Generates a key in accordance with the RFC recommened lenght for each algorithm
+        /// </summary>
+        /// <param name="mode">HashMode</param>
+        /// <returns>Key</returns>
+        public static byte[] GenerateKey(OtpHashMode mode)
+        {
+            return GenerateKey(LengthForMode(mode));
+        }
+
+        private static int LengthForMode(OtpHashMode mode)
+        {
+            switch(mode)
+            {
+                case OtpHashMode.Sha256:
+                    return 32;
+                case OtpHashMode.Sha512:
+                    return 64;
+                default:
+                case OtpHashMode.Sha1:
+                    return 20;
+            }
+        }
+    }
+}
