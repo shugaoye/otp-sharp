539dd7bcf1eab656103e9d5265a2e06d6cb2c5a3
devin <devin@localhost>
2012-08-21 08:53:08 -0600

    increase key generation test coverage to 100%

---
+++ OtpSharp.Tests/KeyGenerationTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OtpSharp.Tests
+{
+    [TestClass]
+    public class KeyGenerationTests
+    {
+        [TestMethod]
+        public void LenghtForMode_Sha1()
+        {
+            Assert.AreEqual(20, KeyGeneration.GenerateKey(OtpHashMode.Sha1).Length);
+        }
+
+        [TestMethod]
+        public void LenghtForMode_Sha256()
+        {
+            Assert.AreEqual(32, KeyGeneration.GenerateKey(OtpHashMode.Sha256).Length);
+        }
+
+        [TestMethod]
+        public void LenghtForMode_Sha512()
+        {
+            Assert.AreEqual(64, KeyGeneration.GenerateKey(OtpHashMode.Sha512).Length);
+        }
+
+        [TestMethod]
+        public void GenerageKey_Zero()
+        {
+            Assert.AreEqual(0, KeyGeneration.GenerateKey(0).Length);
+        }
+
+        [TestMethod]
+        public void GenerageKey_Ten()
+        {
+            Assert.AreEqual(10, KeyGeneration.GenerateKey(10).Length);
+        }
+    
+    }
+}
