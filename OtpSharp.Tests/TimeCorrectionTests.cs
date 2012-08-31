2d8d7b9b952f94c2a1f5cb2de23d0de756256861
devin <devin@localhost>
2012-08-31 13:39:08 -0600

    Add tests to cover the new TOTP correction options.

--- OtpSharp.Tests/TimeCorrectionTests.cs
+++ OtpSharp.Tests/TimeCorrectionTests.cs
@@ -4,6 +4,10 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace OtpSharp.Tests
 {
+    /// <remarks>
+    /// Warning! This is potentially a brittle test as it relies on the completion of the test body within a specific time.
+    /// Every effort has been taken to make these tests not fail when they shouldn't.
+    /// </remarks>
     [TestClass]
     public class TimeCorrectionTests
     {
@@ -18,10 +22,6 @@ namespace OtpSharp.Tests
             Assert.AreEqual(baseTime, correction.GetCorrectedTime(incorrectReferenceTime));
         }
 
-        /// <remarks>
-        /// Warning! This is potentially a brittle test as it relies on the completion of the test body within a specific time.
-        /// 500 ms should be plenty but it is still a brittle test.
-        /// </remarks>
         [TestMethod]
         public void TimeCorrection_Basic()
         {
@@ -39,6 +39,67 @@ namespace OtpSharp.Tests
         {
             var baseTime = DateTime.UtcNow;
             Assert.AreEqual(baseTime, TimeCorrection.UncorrectedInstance.GetCorrectedTime(baseTime));
+        }
+
+        [TestMethod]
+        public void TimeCorrection_TotpWithCorrectionRemainingSeconds()
+        {
+            var correction = new TimeCorrection(DateTime.UtcNow.AddSeconds(5));
+
+            var totp = new Totp(OtpCalculationTests.rfcTestKey);
+            var correctedTotp = new Totp(OtpCalculationTests.rfcTestKey, timeCorrection: correction);
+
+            var standardRemaining = totp.RemainingSeconds();
+            var correctedRemaining = correctedTotp.RemainingSeconds();
+
+            Assert.AreNotEqual(standardRemaining, correctedRemaining, "Correction wasn't applied");
+
+            int difference = 0;
+            if (standardRemaining > correctedRemaining)
+                difference = standardRemaining - correctedRemaining;
+            else
+                difference = (standardRemaining + 30) - correctedRemaining;
+
+            Assert.AreEqual(5, difference);
+        }
+
+        [TestMethod]
+        public void TimeCorrection_TotpWithCorrectionGeneration()
+        {
+            var correction = new TimeCorrection(DateTime.UtcNow.AddSeconds(100)); // 100 ensures that at a minimum we are 3 steps away
+
+            var correctedTotp = new Totp(OtpCalculationTests.rfcTestKey, timeCorrection: correction);
+
+            // make sure we don't run over the window
+            while (correctedTotp.RemainingSeconds() == 0)
+                Thread.Sleep(100);
+
+            // since the compute totp overload that takes a specific time doesn't apply correction we don't need to TOTP objects
+            var uncorrectedCode = correctedTotp.ComputeTotp(DateTime.UtcNow.AddSeconds(100));
+            var correctedCode = correctedTotp.ComputeTotp();
+
+            Assert.AreEqual(uncorrectedCode, correctedCode);
+        }
+
+        [TestMethod]
+        public void TimeCorrection_TotpWithCorrectionVerification()
+        {
+            var correction = new TimeCorrection(DateTime.UtcNow.AddSeconds(100)); // 100 ensures that at a minimum we are 3 steps away
+
+            var correctedTotp = new Totp(OtpCalculationTests.rfcTestKey, timeCorrection: correction);
+
+            // make sure we don't run over the window
+            while (correctedTotp.RemainingSeconds() == 0)
+                Thread.Sleep(100);
+
+            var code = correctedTotp.ComputeTotp();
+
+            long correctedStep, uncorrectedStep;
+
+            Assert.IsTrue(correctedTotp.VerifyTotp(code, out correctedStep));
+            Assert.IsTrue(correctedTotp.VerifyTotp(DateTime.UtcNow.AddSeconds(100), code, out uncorrectedStep));
+
+            Assert.AreEqual(uncorrectedStep, correctedStep);
         }
     }
 }
