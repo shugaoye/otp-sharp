3ec9a8e2689ca5d7b5196f7a7fe28010bf3ae1e4
devin <devin@localhost>
2012-09-24 17:01:40 -0600

    Add determanistic key derivation (without tests)

--- OtpSharp/KeyGeneration.cs
+++ OtpSharp/KeyGeneration.cs
@@ -1,5 +1,7 @@
-﻿
-using System;
+﻿using System;
+using System.Security.Cryptography;
+using System.Linq;
+
 namespace OtpSharp
 {
     /// <summary>
@@ -52,9 +54,55 @@ namespace OtpSharp
             return GenerateRandomKey(LengthForMode(mode));
         }
 
+        /// <summary>
+        /// Uses the procedure defined in RFC 4226 section 7.5 to derive a key from the master key
+        /// </summary>
+        /// <param name="masterKey">The master key from which to derive a device specific key</param>
+        /// <param name="publicIdentifier">The public identifier that is unique to the authenticating device</param>
+        /// <param name="mode">The hash mode to use.  This will determine the resulting key lenght.  The default is sha-1 (as per the RFC) which is 20 bytes</param>
+        /// <returns>Derived key</returns>
+        public static byte[] DeriveKeyFromMaster(Key masterKey, byte[] publicIdentifier, OtpHashMode mode = OtpHashMode.Sha1)
+        {
+            byte[] key = null;
+            masterKey.UsePlainKey(plainMasterKey =>
+            {
+                var hashAlgorithm = GetHashAlgorithmForMode(mode);
+                var masterKeyWithPublicIdentifier = Combine(plainMasterKey, publicIdentifier);
+                key = hashAlgorithm.ComputeHash(masterKeyWithPublicIdentifier);
+                Destroy(masterKeyWithPublicIdentifier);
+            });
+
+            return key;
+        }
+
+        /// <summary>
+        /// Uses the procedure defined in RFC 4226 section 7.5 to derive a key from the master key
+        /// </summary>
+        /// <param name="masterKey">The master key from which to derive a device specific key</param>
+        /// <param name="serialNumber">A serial number that is unique to the authenticating device</param>
+        /// <param name="mode">The hash mode to use.  This will determine the resulting key lenght.  The default is sha-1 (as per the RFC) which is 20 bytes</param>
+        /// <returns>Derived key</returns>
+        public static byte[] DeriveKeyFromMaster(Key masterKey, int serialNumber, OtpHashMode mode = OtpHashMode.Sha1)
+        {
+            return DeriveKeyFromMaster(masterKey, BitConverter.GetBytes(serialNumber), mode);
+        }
+
+        private static HashAlgorithm GetHashAlgorithmForMode(OtpHashMode mode)
+        {
+            switch (mode)
+            {
+                case OtpHashMode.Sha256:
+                    return new SHA256Managed();
+                case OtpHashMode.Sha512:
+                    return new SHA512Managed();
+                default: //case OtpHashMode.Sha1:
+                    return new SHA1Managed();
+            }
+        }
+
         private static int LengthForMode(OtpHashMode mode)
         {
-            switch(mode)
+            switch (mode)
             {
                 case OtpHashMode.Sha256:
                     return 32;
@@ -64,5 +112,26 @@ namespace OtpSharp
                     return 20;
             }
         }
+
+        #region key utilities
+
+        internal static byte[] Combine(params byte[][] arrays)
+        {
+            byte[] rv = new byte[arrays.Sum(a => a.Length)];
+            int offset = 0;
+            foreach (byte[] array in arrays)
+            {
+                System.Buffer.BlockCopy(array, 0, rv, offset, array.Length);
+                offset += array.Length;
+            }
+            return rv;
+        }
+
+        internal static void Destroy(byte[] sensetiveData)
+        {
+
+        }
+
+        #endregion
     }
 }
