2f47be3ffe2b894054f537ee6c1c5616aaad070f
devin <devin@localhost>
2013-07-29 12:41:32 -0600

    Add nuget dependency for Base32 package

--- OtpSharp/Properties/AssemblyInfo.cs
+++ OtpSharp/Properties/AssemblyInfo.cs
@@ -31,8 +31,8 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 // The CI server will provide the Build Number and Revision
-[assembly: AssemblyVersion("1.1.*")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.*")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
 
 // allow the test assembly to unit test internals
 [assembly: InternalsVisibleTo("OtpSharp.Tests")]
