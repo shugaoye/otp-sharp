6887577764e6fcd8176c34e0ce1576f8d16f2371
Devin Martin <devin@devin-martin.com>
2014-07-17 03:30:24 +0000

    Windows store stub

---
+++ OtpSharp.WindowsStore/InMemoryKey.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtpSharp
+{
+    public class InMemoryKey : IKeyProvider
+    {
+        public InMemoryKey(byte[] secret)
+        { }
+
+        byte[] IKeyProvider.ComputeHmac(OtpHashMode mode, byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
